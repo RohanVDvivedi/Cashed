c12fb447ac6ae692e460885b12adb12139584ac0
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-10-09 16:11:39 +0530

    a better understanding for the new functionality I am going to provide, it is easier to work on, the PROGRAM datatype and the EXEC command will help solve a lot of problems of distributed stream processing systems

--- inc/command.h
+++ inc/command.h
@@ -4,10 +4,21 @@
 typedef enum command command;
 enum command
 {
+	// for the main hash table and intenal hashes, bsts
 	GET,
 	SET,
+
+	// for internal queues, stacks and heaps
+	PUSH,
+	POP,
+
+	// crerate a a new data structure type for your purpose
 	NEW,
+
+	// delete the data structure and all its internals
 	DEL,
+
+	// this command can execute a PRAGRAM datatye, and a result is returned to you
 	EXEC
 };
 
--- inc/data.h
+++ inc/data.h
@@ -39,20 +39,20 @@ enum DataType
 	NUM_INTEGER,
 
 	// double
-	NUM_FLOAT//,
+	NUM_FLOAT,
 
 	// a number stored as string
-	// NUM_DECIMAL,
+	NUM_DECIMAL,
 
 	// unsigned long long int, storing the nanoseconds since epoch
-	// TIME_STAMP,
+	TIME_STAMP,
 
 	// this is of type unsigned character, it maps to a string stored in system tables
-	// ENUMERATION,
+	ENUMERATION,
 
 	// this is address to another data, it can be in any other node, and any where
 	// yet to be defined properly
-	// POINTER
+	POINTER
 
 	// code that can be executed
 	PROGRAM
