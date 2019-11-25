de3acd7a896c477f5ddeba9836b0ae9334188022
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-11-26 01:30:10 +0530

    removing bugs from connection variables

--- inc/connection_variables.h
+++ inc/connection_variables.h
@@ -1,15 +1,18 @@
 #ifndef CONNECTION_VARIABLES_H
 #define CONNECTION_VARIABLES_H
 
+#include<stdlib.h>
+
+#include<jenkinshash.h>
 #include<global_hash.h>
 
 hashmap* get_connection_variables_hashmap();
 
-void insert_connection_variable(dstring* variable_name, int is_shared, Data* value);
+void set_connection_variable(hashmap* connection_variables, dstring* var_name, int is_shared, Data* value);
 
-Data* get_connection_variable(dstring* variable_name);
+Data* get_connection_variable(hashmap* connection_variables, dstring* var_nam);
 
-int delete_connection_variable(dstring* variable_name);
+int delete_connection_variable(hashmap* connection_variables, dstring* var_nam);
 
 void delete_connection_variables_hashmap(hashmap* connection_variables);
 
--- src/connection_variables.c
+++ src/connection_variables.c
@@ -1,26 +1,79 @@
 #include<connection_variables.h>
 
+typedef struct variable_name variable_name;
+struct variable_name
+{
+	dstring* name;
+	int is_shared;
+};
+
+variable_name* get_variable_name(char* name, int is_shared)
+{
+	variable_name* var_nam = malloc(sizeof(variable_name));
+	var_nam->name = get_dstring(name, 10);
+	var_nam->is_shared = is_shared;
+	return var_nam;
+}
+
+void delete_variable_name(variable_name* var_nam)
+{
+	delete_dstring(var_nam->name);
+	free(var_nam);
+}
+
+unsigned long long int variable_name_hash(variable_name* var_nam)
+{
+	return jenkins_hash(var_nam->name->cstring, var_nam->name->bytes_occupied);
+}
+
+int variable_name_compare(variable_name* var_nam1, variable_name* var_nam2)
+{
+	return compare_dstring(var_nam1->name, var_nam2->name);
+}
+
 hashmap* get_connection_variables_hashmap()
 {
-	// todo
-	return NULL;
+	hashmap* connection_variables = get_hashmap(10, (unsigned long long int (*)(const void*))variable_name_hash, (int (*)(const void*, const void*))variable_name_compare, ELEMENTS_AS_RED_BLACK_BST);
+	insert_entry_in_hash(connection_variables, get_variable_name("global_hash", 1), global_hash);
+	return connection_variables;
 }
 
-void insert_connection_variable(dstring* variable_name, int is_shared, Data* value)
+void set_connection_variable(hashmap* connection_variables, dstring* var_nam, int is_shared, Data* value)
 {
-	// todo
+	variable_name* var_nam_key = get_variable_name(var_nam->cstring, is_shared);
+	if(find_value_from_hash(connection_variables, var_nam_key) == NULL)
+	{
+		insert_entry_in_hash(connection_variables, var_nam_key, value);
+	}
+	else
+	{
+		update_value_in_hash(connection_variables, var_nam_key, value, NULL);
+		delete_variable_name(var_nam_key);
+	}
 }
 
-Data* get_connection_variable(dstring* variable_name)
+Data* get_connection_variable(hashmap* connection_variables, dstring* var_nam)
 {
-	// todo
-	return NULL;
+	variable_name var_nam_key = {.name = var_nam, .is_shared = 1};
+	Data* result = (Data*)find_value_from_hash(connection_variables, &var_nam_key);
+	return result;
 }
 
-int delete_connection_variable(dstring* variable_name)
+int delete_connection_variable(hashmap* connection_variables, dstring* var_nam)
 {
-	// todo
-	return 0;
+	variable_name var_nam_key = {.name = var_nam, .is_shared = 1};
+	variable_name* return_key = NULL;
+	Data* return_value = NULL;
+	int result = delete_entry_from_hash(connection_variables, &var_nam_key, (const void**)(&return_key), (const void**)(&return_value));
+	if(result > 0)
+	{
+		if(return_key->is_shared == 0)
+		{
+			delete_data(return_value);
+		}
+		delete_variable_name(return_key);
+	}
+	return result;
 }
 
 void delete_connection_variables_hashmap(hashmap* connection_variables)
