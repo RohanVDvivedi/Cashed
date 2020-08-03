746bb9fb49df8e8d9a8789cd3da7f601d408a508
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-08-03 23:26:57 +0530

    built basic interface for the client that will be provided for cashed

--- inc/cashed_client.h
+++ inc/cashed_client.h
@@ -1,4 +1,17 @@
 #ifndef CASHED_CLIENT_H
 #define CASHED_CLIENT_H
 
+#include<transaction_client.h>
+
+// only tcp/ipv4
+transaction_client* get_cashed_client(char* host, int port, int connection_count);
+
+dstring* GET_cashed(transaction_client* cashed_client, dstring* key);
+
+int SET_cashed(transaction_client* cashed_client, dstring* key, dstring* value);
+
+int DEL_cashed(transaction_client* cashed_client, dstring* key);
+
+void close_cashed_client(transaction_client* cashed_client);
+
 #endif
--- inc/cashed_server.h
+++ inc/cashed_server.h
@@ -1,7 +1,7 @@
 #ifndef CASHED_SERVER_H
 #define CASHED_SERVER_H
 
+// only tcp/ipv4
 int start_cashed_server(int port);
-
 
 #endif
