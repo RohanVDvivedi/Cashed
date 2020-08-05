19d75554504af0f2a1c750eb0964ceaeb9ac9462
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-08-05 19:44:26 +0530

    implemented copy and read methodds for data

--- inc/data.h
+++ inc/data.h
@@ -22,18 +22,27 @@ struct data
 	// value = key_value[key_size]
 };
 
+// you must take appropriate lock on the data_lock before calling any of the below function that modifies or reads data
+
 unsigned int size_of_data(const dstring* key, const dstring* value);
 
 void init_data(data* data_p, const dstring* key, const dstring* value);
 
-void append_key(const data* data_p, dstring* key);
+// updates content from value to data->value
+void update_value(data* data_p, const dstring* value);
 
-void append_value(const data* data_p, dstring* value);
+// appends the content of data->key to key
+void append_key(const data* data_p, dstring* append_to);
+
+// appends the content of data->key to key
+void append_value(const data* data_p, dstring* append_to);
 
 void init_dummy_data(data* data_p, char* key);
 
 int compare_data(const data* data_p1, const data* data_p2);
 
 unsigned int hash_data(const data* data_p);
+
+void deinit_data(data* data_p);
 
 #endif
--- src/data.c
+++ src/data.c
@@ -11,21 +11,29 @@ void init_data(data* data_p, const dstring* key, const dstring* value)
 {
 	data_p->hashtable_next = NULL;
 
-	data_p->key_size = key->bytes_occupied - 1;
-	data_p->value_size = value->bytes_occupied - 1;
+	initialize_rwlock(&(data_p->data_lock));
 
+	data_p->key_size = key->bytes_occupied - 1;
 	memcpy(data_p->key_value, key->cstring, data_p->key_size);
+
+	data_p->value_size = value->bytes_occupied - 1;
 	memcpy(data_p->key_value + data_p->key_size, value->cstring, data_p->value_size);
 }
 
-void append_key(const data* data_p, dstring* key)
+void update_value(data* data_p, const dstring* value)
 {
-	appendn_to_dstring(key, data_p->key_value, data_p->key_size);
+	data_p->value_size = value->bytes_occupied - 1;
+	memcpy(data_p->key_value + data_p->key_size, value->cstring, data_p->value_size);
 }
 
-void append_value(const data* data_p, dstring* value)
+void append_data_key(const data* data_p, dstring* append_to)
 {
-	appendn_to_dstring(key, data_p->key_value + data_p->key_size, data_p->value_size);
+	appendn_to_dstring(append_to, (char*)(data_p->key_value), data_p->key_size);
+}
+
+void append_data_value(const data* data_p, dstring* append_to)
+{
+	appendn_to_dstring(append_to, (char*)(data_p->key_value + data_p->key_size), data_p->value_size);
 }
 
 void init_dummy_data(data* data_p, char* key)
@@ -49,4 +57,9 @@ int compare_data(const data* data_p1, const data* data_p2)
 unsigned int hash_data(const data* data_p)
 {
 	return jenkins_hash(data_p->key_value, data_p->key_size);
+}
+
+void deinit_data(data* data_p)
+{
+	deinitialize_rwlock(&(data_p->data_lock));
 }
