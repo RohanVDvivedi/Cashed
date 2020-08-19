74f5cef754329c3dfea053b574e4f7762a297c20
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-08-19 18:07:03 +0530

    minor issue resolved

--- src/cashed_hashtable.c
+++ src/cashed_hashtable.c
@@ -31,6 +31,45 @@ void init_cashbucket(cashbucket* bucket)
 	bucket->data_list = NULL;
 }
 
+static data* find_cashbucket_data_by_key_unsafe(cashbucket* bucket, const dstring* key, data** prev_return)
+{
+	data* data_test = bucket->data_list;
+	data* prev = NULL;
+	while(data_test != NULL && compare_key(data_test, key) != 0)
+	{
+		prev = data_test;
+		data_test = data_test->h_next;
+	}
+	if(prev_return != NULL)
+		*prev_return = prev;
+	return data_test;
+}
+
+static void insert_cashbucket_head_unsafe(cashbucket* bucket, data* new_data)
+{
+	new_data->h_next = bucket->data_list;
+	bucket->data_list = new_data;
+}
+
+// if prev is NULL, then we remove head
+// returns the removed node
+static data* remove_cashbucket_data_next_of_unsafe(cashbucket* bucket, data* prev)
+{
+	data* data_ret = NULL;
+	if(prev == NULL)
+	{
+		data_ret = bucket->data_list;
+		bucket->data_list = bucket->data_list->h_next;
+	}
+	else
+	{
+		data_ret = prev->h_next;
+		prev->h_next = prev->h_next->h_next;
+	}
+	data_ret->h_next = NULL;
+	return data_ret;
+}
+
 void deinit_cashbucket(cashbucket* bucket)
 {
 	deinitialize_rwlock(&(bucket->data_list_lock));
@@ -63,18 +102,22 @@ int get_cashtable(cashtable* cashtable_p, const dstring* key, dstring* return_va
 {
 	unsigned int index = jenkins_hash_dstring(key) % cashtable_p->bucket_count;
 	cashbucket* bucket = cashtable_p->buckets + index;
+
 	read_lock(&(bucket->data_list_lock));
-		data* data_test = bucket->data_list;
-		while(data_test != NULL && compare_key(data_test, key) != 0){data_test = data_test->h_next;}
-		if(data_test != NULL)
-			read_lock(&(data_test->data_value_lock));
-	read_unlock(&(bucket->data_list_lock));
-		if(data_test != NULL)
-		{
-				append_data_value(data_test, return_value);
-			read_unlock(&(data_test->data_value_lock));
-		}
-	return data_test != NULL;
+
+	data* data_found = find_cashbucket_data_by_key_unsafe(bucket, key, NULL);
+
+	if(data_found != NULL)
+	{
+		read_lock(&(data_found->data_value_lock));
+		read_unlock(&(bucket->data_list_lock));
+		append_data_value(data_found, return_value);
+		read_unlock(&(data_found->data_value_lock));
+	}
+	else
+		read_unlock(&(bucket->data_list_lock));
+
+	return data_found != NULL;
 }
 
 int set_cashtable(cashtable* cashtable_p, const dstring* key, const dstring* value)
@@ -83,44 +126,40 @@ int set_cashtable(cashtable* cashtable_p, const dstring* key, const dstring* val
 	cashbucket* bucket = cashtable_p->buckets + index;
 	
 	write_lock(&(bucket->data_list_lock));
-		data* data_test = bucket->data_list;
-		data* prev = NULL;
 
-		while(data_test != NULL && compare_key(data_test, key) != 0){prev = data_test;data_test = data_test->h_next;}
+	data* prev = NULL;
+	data* data_found = find_cashbucket_data_by_key_unsafe(bucket, key, &prev);
 		
-		unsigned int size_of_new_data = size_of_data(key, value);
-		int new_allocation_and_insertion_required = 0;
+	unsigned int size_of_new_data = size_of_data(key, value);
+	int new_allocation_and_insertion_required = 0;
 
-		if(data_test != NULL)
+	if(data_found != NULL)
+	{
+		if(data_found->total_data_size < size_of_new_data)
 		{
-			if(data_test->total_data_size < size_of_new_data)
-			{
-				if(prev != NULL)
-					prev->h_next = data_test->h_next;
-				else
-					bucket->data_list = data_test->h_next;
-				data_test->h_next = NULL;
+			remove_cashbucket_data_next_of_unsafe(bucket, prev);
+			free(data_found);
 
-				new_allocation_and_insertion_required = 1;
-			}
-			else
-			{
-				write_lock(&(data_test->data_value_lock));
-					update_value(data_test, value);
-				write_unlock(&(data_test->data_value_lock));
-			}
+			new_allocation_and_insertion_required = 1;
 		}
 		else
-			new_allocation_and_insertion_required = 1;
-
-		if(new_allocation_and_insertion_required)
 		{
-			data* new_data = malloc(size_of_new_data);
-			init_data(new_data, size_of_new_data, key, value);
-			new_data->h_next = bucket->data_list;
-			bucket->data_list = new_data;
+			write_lock(&(data_found->data_value_lock));
+			write_unlock(&(bucket->data_list_lock));
+			update_value(data_found, value);
+			write_unlock(&(data_found->data_value_lock));
 		}
-	write_unlock(&(bucket->data_list_lock));
+	}
+	else
+		new_allocation_and_insertion_required = 1;
+
+	if(new_allocation_and_insertion_required)
+	{
+		data* new_data = malloc(size_of_new_data);
+		init_data(new_data, size_of_new_data, key, value);
+		insert_cashbucket_head_unsafe(bucket, new_data);
+		write_unlock(&(bucket->data_list_lock));
+	}
 
 	return 1;
 }
@@ -131,28 +170,24 @@ int del_cashtable(cashtable* cashtable_p, const dstring* key)
 	cashbucket* bucket = cashtable_p->buckets + index;
 
 	write_lock(&(bucket->data_list_lock));
-		data* data_test = bucket->data_list;
-		data* prev = NULL;
-		while(data_test != NULL && compare_key(data_test, key) != 0){prev = data_test;data_test = data_test->h_next;}
-		if(data_test != NULL)
-		{
-			if(prev != NULL)
-				prev->h_next = data_test->h_next;
-			else
-				bucket->data_list = data_test->h_next;
-			data_test->h_next = NULL;
-		}
+
+	data* prev = NULL;
+	data* data_found = find_cashbucket_data_by_key_unsafe(bucket, key, &prev);
+
+	if(data_found != NULL)
+		remove_cashbucket_data_next_of_unsafe(bucket, prev);
+
 	write_unlock(&(bucket->data_list_lock));
 
-	if(data_test != NULL)
+	if(data_found != NULL)
 	{
 		// taking a write lock ensures that noone could be working on this data, now
-		write_lock(&(data_test->data_value_lock));
-			deinit_data(data_test);
-			free(data_test);
+		write_lock(&(data_found->data_value_lock));
+		deinit_data(data_found);
+		free(data_found);
 	}
 
-	return data_test != NULL;
+	return data_found != NULL;
 }
 
 void deinit_cashtable(cashtable* cashtable_p)
