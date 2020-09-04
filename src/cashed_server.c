320f6bdc4ec31ce444aafdcaaf8230a9d7c695c7
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-09-05 02:17:39 +0530

    removed debug statements

--- src/cashed_server.c
+++ src/cashed_server.c
@@ -47,9 +47,6 @@ void connection_handler(int conn_fd, void* cashtable_p_v)
 				appendn_to_dstring(&io_string, buffer, buffreadlength);
 			}
 		}
-		printf("%d %d => ", io_error, semicolon_received);
-		display_dstring(&io_string);
-		printf("\n");
 
 		if(!io_error)
 		{
@@ -59,8 +56,6 @@ void connection_handler(int conn_fd, void* cashtable_p_v)
 
 			// build the query from the data that we read
 			deserialize_query(&io_string, &q);
-			display_dstring(&io_string);
-			print_query(&q);
 
 			// process the query, and get result in the io_string
 			process_query(cashtable_p, &q, &r);
@@ -69,8 +64,6 @@ void connection_handler(int conn_fd, void* cashtable_p_v)
 			make_dstring_empty(&io_string);
 			// parse the io_string to buld the query object
 			serialize_result(&io_string, &r);
-			display_dstring(&io_string);
-			print_result(&r);
 
 			// deinitialize query and result
 			deinit_query(&q);
