325538e0a1bfdb1d365f04ea988e02c4665e0b11
rohan <rohandvivedi@gmail.com>
2021-05-24 20:09:20 +0530

    reading directly from socket into the io dstring reducing copy operation

--- src/cashed_server.c
+++ src/cashed_server.c
@@ -6,62 +6,60 @@
 #include<cashed_hashtable_def.h>
 #include<cashed_process_query.h>
 
-#define QUERY_BUFFER_SIZE 1024
+#define QUERY_BUFFER_EXP_SIZE 1024
 
 void connection_handler(int conn_fd, void* cashtable_p_v)
 {
 	cashtable* cashtable_p = cashtable_p_v;
 
-	// we do not accept commands greater than 1000 characters
-	char buffer[QUERY_BUFFER_SIZE];
-
 	int io_error = 0;
-	dstring io_string;
-	init_dstring(&io_string, NULL, 0);
 
-	// this is the query we build for every request, and its result that we will send
-	c_query q = {};
-	c_result r = {};
+	dstring io_string;
+	init_empty_dstring(&io_string, QUERY_BUFFER_EXP_SIZE);
 
 	while(!io_error)
 	{
-		// clear the io_string holding the query
-		make_dstring_empty(&io_string);
-
 		int semicolon_received = 0;
 		while(!io_error && !semicolon_received)
 		{
+			if(io_string.bytes_allocated - io_string.bytes_occupied < QUERY_BUFFER_EXP_SIZE)
+				expand_dstring(&io_string, io_string.bytes_occupied + QUERY_BUFFER_EXP_SIZE);
+
 			// read data and write it to io_string
-			int buffreadlength = recv(conn_fd, buffer, QUERY_BUFFER_SIZE, 0);
+			int buffreadlength = recv(conn_fd, io_string.cstring + io_string.bytes_occupied, io_string.bytes_allocated - io_string.bytes_occupied, 0);
 
 			if(buffreadlength == -1 || buffreadlength == 0)
 				io_error = 1;
 			else
 			{
-				int iter = 0;
-				while(iter < buffreadlength && !semicolon_received)
+				unsigned int iter = io_string.bytes_occupied;
+
+				// increment the bytes occupied on the io dstring
+				io_string.bytes_occupied += buffreadlength;
+
+				while(iter < io_string.bytes_occupied && !semicolon_received)
 				{
-					if(buffer[iter++] == ';')
+					if(io_string.cstring[iter++] == ';')
 						semicolon_received = 1;
 				}
-				concatenate_dstring(&io_string, dstring_DUMMY_DATA(buffer, buffreadlength));
 			}
 		}
 
 		if(!io_error)
 		{
-			// initialize query and result
-			init_query(&q, ERR);
-			init_result(&r);
+			// init query and result
+			c_query q;		init_query(&q, ERR);
+			c_result r;		init_result(&r);
 
-			// build the query from the data that we read
+			// build the query from the what we read
 			deserialize_query(&io_string, &q);
+
+			// clear the io_string
+			make_dstring_empty(&io_string);
 
 			// process the query, and get result in the io_string
 			process_query(cashtable_p, &q, &r);
 
-			// clear the io_string holding the query
-			make_dstring_empty(&io_string);
 			// parse the io_string to buld the query object
 			serialize_result(&io_string, &r);
 
@@ -74,6 +72,10 @@ void connection_handler(int conn_fd, void* cashtable_p_v)
 			if(buffsentlength == -1 || buffsentlength == 0)
 				io_error = 1;
 		}
+
+		// clear the io_string
+		make_dstring_empty(&io_string);
+		shrink_dstring(&io_string);
 	}
 
 	deinit_dstring(&io_string);
