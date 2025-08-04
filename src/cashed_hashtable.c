ea6003d09c600224edb261f53a2e0e120b168d36
RohanVDvivedi <rohandvivedi@gmail.com>
2025-08-04 21:28:06 +0530

    added declarations for internal functions

--- src/cashed_hashtable.c
+++ src/cashed_hashtable.c
@@ -1,3 +1,18 @@
 #include<cashed/cashed_hashtable.h>
 
 #include<cashed/cashed_data.h>
+
+/*
+	INTERNAL FUNCTIONS - START
+*/
+
+cashed_data* get_from_cashed_hashtable_INTERNAL(cashed_hashtable* chtbl, const cashed_data* data);
+
+// below function does not care about duplicates
+int insert_in_cashed_hashtable_INTERNAL(cashed_hashtable* chtbl, cashed_data* data);
+
+int remove_from_cashed_hashtable_INTERNAL(cashed_hashtable* chtbl, cashed_data* data);
+
+/*
+	INTERNAL FUNCTIONS - END
+*/
