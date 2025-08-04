6c2865c9c4f2c094eb92c4c369b337553bcc650b
RohanVDvivedi <rohandvivedi@gmail.com>
2025-08-04 21:55:36 +0530

    added basic implementation for the cashed_hashtable

--- src/cashed_hashtable.c
+++ src/cashed_hashtable.c
@@ -2,16 +2,41 @@
 
 #include<cashed/cashed_data.h>
 
+#include<posixutils/pthread_cond_utils.h>
+
 /*
 	INTERNAL FUNCTIONS - START
 */
 
-cashed_data* get_from_cashed_hashtable_INTERNAL(cashed_hashtable* chtbl, const cashed_data* data);
+cashed_data* get_from_cashed_hashtable_INTERNAL(cashed_hashtable* chtbl, const cashed_data* data)
+{
+	cashed_data* result = (cashed_data*) find_equals_in_cachemap(&(chtbl->lruhashtable), data);
+	bump_element_in_cachemap(&(chtbl->lruhashtable), result);
+	return result;
+}
 
-// below function does not care about duplicates
-int insert_in_cashed_hashtable_INTERNAL(cashed_hashtable* chtbl, cashed_data* data);
+int insert_in_cashed_hashtable_INTERNAL(cashed_hashtable* chtbl, cashed_data* data)
+{
+	if(!insert_in_cachemap(&(chtbl->lruhashtable), data))
+		return 0;
+	if(data->absolute_expiry_in_microseconds != BLOCKING)
+	{
+		push_to_pheap(&(chtbl->expiryheap), data);
+		if(data == get_top_of_pheap(&(chtbl->expiryheap))) // if the inserted element is the top element for expiry, then wake up alarm job
+			wake_up_alarm_job(chtbl->expiry_manager);
+	}
+	return 1;
+}
 
-int remove_from_cashed_hashtable_INTERNAL(cashed_hashtable* chtbl, cashed_data* data);
+int remove_from_cashed_hashtable_INTERNAL(cashed_hashtable* chtbl, cashed_data* data)
+{
+	if(!remove_from_cachemap(&(chtbl->lruhashtable), data))
+		return 0;
+	int is_data_top_of_expiry_pheap = (data == get_top_of_pheap(&(chtbl->expiryheap))); // if the element was the top element for expiry, then wake up alarm job
+	if(remove_from_pheap(&(chtbl->expiryheap), data) && is_data_top_of_expiry_pheap)
+		wake_up_alarm_job(chtbl->expiry_manager);
+	return 1;
+}
 
 /*
 	INTERNAL FUNCTIONS - END
